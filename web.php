3c9c188cb3a9ec7ac15b4fc4ad0aa5c0d5570fb7
Lionel-prinsloo <133089423+Lionel-prinsloo@users.noreply.github.com>
2023-10-29 13:00:28 +0100

    opdracht 2

--- web.php
+++ web.php
@@ -1,19 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//opdracht 2
+$planets = [
+    [
+        'name' => 'Mars',
+        'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+    ],
+    [
+        'name' => 'Venus',
+        'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+    ],
+    [
+        'name' => 'Earth',
+        'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that is inhabited by living things.'
+    ],
+    [
+        'name' => 'Jupiter',
+        'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+    ],
+];
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+//opdrach 1
 Route::get('/', function () {
     $planeten = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
-    return $planeten;
+    return view('planets', ['planets' => $planeten]);
 });
+
