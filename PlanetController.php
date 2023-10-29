5a06b9439fafd45f335ec65d01cd490b9fda5bd2
Lionel-prinsloo <133089423+Lionel-prinsloo@users.noreply.github.com>
2023-10-29 13:08:39 +0100

    opdracht 4

---
+++ PlanetController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlanetController extends Controller
+{
+    public function index()
+    {
+        // Logic om alle planeten weer te geven
+    }
+
+    public function show($planet)
+    {
+        // Logic om een enkele planeet weer te geven op basis van $planet
+    }
+}
